30ca50aa4ee54d3c54425fc085e67eb7723321b3
Chuck <chidumechukwuemelie@gmail.com>
2021-10-17 11:08:59 -0700

    pointed index.js to the correct location of the todo app

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/lesson48/App';
+import App from './components/todoAppPhase7/App';
 
 ReactDOM.render(<App />, document.querySelector('#root'));
